0865fe3e594847482502318d489d26fa85c2a661
Tomas Gonzalez <cesartomas2003@gmail.com>
2025-05-26 16:53:12 -0300

    Interface DetectorDeSpam añadido al service y testeado usando un lambda

---
+++ src/main/java/ar/edu/utn/frba/dds/DetectorSpam/DetectorDeSpam.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.frba.dds.DetectorSpam;
+
+public interface DetectorDeSpam {
+  boolean esSpam(String texto);
+}
--- src/main/java/ar/edu/utn/frba/dds/servicios/SolicitudService.java
+++ src/main/java/ar/edu/utn/frba/dds/servicios/SolicitudService.java
@@ -1,13 +1,25 @@
 package ar.edu.utn.frba.dds.servicios;
 
+import ar.edu.utn.frba.dds.DetectorSpam.DetectorDeSpam;
 import ar.edu.utn.frba.dds.repositorios.RepositorioSolicitudes;
 import ar.edu.utn.frba.dds.solicitudes.*;
 
 public class SolicitudService {
   private RepositorioSolicitudes repositorio;
 
-  public SolicitudService(RepositorioSolicitudes repositorio) {
+  private DetectorDeSpam detector;
+
+  public SolicitudService(RepositorioSolicitudes repositorio,DetectorDeSpam detector) {
     this.repositorio = repositorio;
+    this.detector = detector;
+  }
+
+  public void procesarNuevaSolicitud(SolicitudEliminacion solicitud) {
+    if (detector.esSpam(solicitud.getFundamentacion())) {
+      solicitud.rechazar();
+    }
+
+    repositorio.actualizar(solicitud);
   }
 
   public void aprobarSolicitud(SolicitudEliminacion solicitud) {
