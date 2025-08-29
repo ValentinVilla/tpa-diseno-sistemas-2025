2d4dc7a32ffec856ba3d73b43cdb45fcf336e7df
tobiasWaizer <twaizer@frba.utn.edu.ar>
2025-08-29 10:38:14 -0300

    consenso absoluto reemplazado por allMatch (falta hacer tests)

--- src/main/java/ar/edu/utn/frba/dds/consenso/ConsensoAbsoluto.java
+++ src/main/java/ar/edu/utn/frba/dds/consenso/ConsensoAbsoluto.java
@@ -9,12 +9,17 @@ public class ConsensoAbsoluto implements AlgoritmoConsenso {
 
   @Override
   public void tieneConsenso(Hecho hecho, List<Fuente> fuentes) {
-    for (Fuente fuente : fuentes) {
-      if (!fuente.cargarHechos(null).contains(hecho)) {
-        hecho.setConsensuado(false);
-        return; // Si al menos una fuente no contiene el hecho, no hay consenso absoluto
-      }
-    }
-    hecho.setConsensuado(true);
+//    for (Fuente fuente : fuentes) {
+//      if (!fuente.cargarHechos(null).contains(hecho)) {
+//        hecho.setConsensuado(false);
+//        return; // Si al menos una fuente no contiene el hecho, no hay consenso absoluto
+//      }
+//    }
+
+    boolean consenso = fuentes.stream().allMatch(fuente -> fuente.cargarHechos(null).contains(hecho));
+    hecho.setConsensuado(consenso);
   }
+
+
+
 }
