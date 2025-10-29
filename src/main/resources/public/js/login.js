ae014344c872359ac182fd60b7670d8fdccde3aa
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2025-10-29 18:52:13 -0300

    base del front de colecciones

--- src/main/resources/public/js/login.js
+++ src/main/resources/public/js/login.js
@@ -1,11 +1,11 @@
-const login = document.getElementById('login'),
-    loginBtn = document.getElementById('login-btn'),
-    loginClose = document.getElementById('login-close')
+const login = document.getElementById("login"),
+  loginBtn = document.getElementById("login-btn"),
+  loginClose = document.getElementById("login-close");
 
-loginBtn.addEventListener('click', () =>{
-    login.classList.add('show-login')
-})
+loginBtn.addEventListener("click", () => {
+  login.classList.add("show-login");
+});
 
-loginClose.addEventListener('click', () =>{
-    login.classList.remove('show-login')
-})
+loginClose.addEventListener("click", () => {
+  login.classList.remove("show-login");
+});
